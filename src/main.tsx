373c2aa6b5f3fa5586de43ad94bb17e7c79b2f93
Manuel Fittko <manuel.fittko@sofatutor.com>
2025-03-10 22:47:10 +0100

    feat: add configurable logging system with timestamps and source info

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ServiceProvider } from './providers/ServiceProvider'
+import { initLogger, overrideConsoleMethods } from './utils/logger'
+
+// Initialize logger and override console methods
+initLogger();
+overrideConsoleMethods();
 
 createRoot(document.getElementById("root")!).render(
   <ServiceProvider>
